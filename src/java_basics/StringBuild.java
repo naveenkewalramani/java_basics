4507f62ba2957bdc0b0e40c2a8ce45fa69aeef63
naveen.kewalramani <naveen.kewalramani2903@gmail.com>
2020-04-10 19:28:10 +0530

    string builder

---
+++ src/java_basics/StringBuild.java
@@ -0,0 +1,10 @@
+package java_basics;
+
+public class StringBuild {
+	public static void main(String [] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("This is naveen");
+		System.out.println(sb.toString());
+	}
+
+}
